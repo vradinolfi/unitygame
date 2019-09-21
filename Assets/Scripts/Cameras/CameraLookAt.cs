e7691e7b06b9ecd0abb318aaba425d85955d697a
Vincent Adinolfi <vincentadinolfi@gmail.com>
2019-09-20 19:54:50 -0500

    rewrote camera lookat script to introduce some smoothing and better structure

--- Assets/Scripts/Cameras/CameraLookAt.cs
+++ Assets/Scripts/Cameras/CameraLookAt.cs
@@ -5,12 +5,25 @@ using UnityEngine;
 public class CameraLookAt : MonoBehaviour
 {
     public Transform target;
-    public int xOffset = 0;
-    public int yOffset = 0;
+    public float xOffset = 0;
+    public float yOffset = 0;
+    public float smoothSpeed = 2f;
+
+    Vector3 offset;
 
     void FixedUpdate()
     {
-        transform.LookAt(target);
-        transform.rotation *= Quaternion.Euler(xOffset, yOffset, 0);
+        offset.Set(xOffset, yOffset, 0);
+
+        var targetRotation = Quaternion.LookRotation((target.transform.position + offset) - transform.position);
+
+        // Smoothly rotate towards the target
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, smoothSpeed * Time.deltaTime);
+
+        // Linear rotation
+        //transform.LookAt(target);
+        //transform.rotation *= Quaternion.Euler(xOffset, yOffset, 0);
+        //transform.rotation *= Quaternion.Euler(xOffset, yOffset, 0);
+
     }
 }
