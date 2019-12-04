e6286bbc779dc4d57c6805b89e820c4386f525a9
Vincent Adinolfi <vincentadinolfi@gmail.com>
2019-12-04 00:20:44 -0600

    more env detailing

---
+++ Assets/Scripts/Tools/TVFlicker.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TVFlicker : MonoBehaviour
+{
+    Light testLight;
+    public float minWaitTime;
+    public float maxWaitTime;
+    public float minIntensity;
+    public float maxIntensity;
+
+    void Start()
+    {
+        testLight = GetComponent<Light>();
+        StartCoroutine(Flashing());
+    }
+
+    IEnumerator Flashing()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(minWaitTime, maxWaitTime));
+            //testLight.enabled = !testLight.enabled;
+            testLight.intensity = Random.Range(minIntensity, maxIntensity);
+
+        }
+    }
+}
